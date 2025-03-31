6c30cf11a2811bb635a6beb6234a923444a4bb10
sonai-2005 <nandisonai60@gmail.com>
2025-04-01 01:10:20 +0530

    add the files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3>A Blog Application using Appwrite</h3>
+    </>
+  )
+}
+
+export default App
